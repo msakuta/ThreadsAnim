7425b33f9af931f1756ca5eab9a3cec7c46a82a2
msakuta <masahiro.sakuta@gmail.com>
2021-09-17 08:52:59 +0900

    Add animation to sit

--- ThreadsAnim.js
+++ ThreadsAnim.js
@@ -4,106 +4,154 @@ var canvas;
 var width;
 var height;
 var animImages = [];
-var deskImage;
-var studentImages = [];
+let deskImage;
+let chairImage;
+let deskBackImage;
+let studentImages = [];
+let studentSitImage;
 
 const animFrames = 10;
 
+const IDLE = 0;
+const WALKING = 1;
+const SIT = 2;
+
 class Person {
-	name = "A";
-	pos = [0, 0];
-	cooldown = 0;
-	balls = [];
-	frame = 0;
+    name = "A";
+    pos = [0, 0];
+    cooldown = 0;
+    balls = [];
+    frame = 0;
+    state = IDLE;
 
-	constructor(params){
-		this.name = params.name;
-		this.pos = params.pos;
-		this.cooldown = params.cooldown;
-		this.balls = params.balls;
-	}
+    constructor(params){
+        this.id = params.id;
+        this.name = params.name;
+        this.pos = params.pos;
+        this.dest = params.dest;
+        this.cooldown = params.cooldown;
+        this.balls = params.balls;
+        this.walking = true;
+    }
 
-	animate(){
-		this.pos[0] = (this.pos[0] + 2) % width;
-		this.frame = (this.frame + 1) % (2 * animFrames);
-	}
+    animate(){
+        if(this.state === WALKING){
+            if(Math.abs(this.pos[0] - this.dest) < 2){
+                this.state = this.dest === 0 ? IDLE : SIT;
+                this.pos[0] = this.dest;
+            }
+            else
+                this.pos[0] = (this.pos[0] + (this.pos[0] < this.dest ? 2 : -2) + width) % width;
+        }
+        else if(this.state === SIT && this.balls.length === 0){
+            if(tasks === 0 && --this.cooldown <= 0){
+                this.dest = 0;
+                this.state = WALKING;
+            }
+        }
+        else if(this.state === IDLE && tasks.length !== 0){
+            this.state = WALKING;
+            this.dest = this.id * 100 + 100;
+        }
+        this.frame = (this.frame + 1) % (2 * animFrames);
+    }
 
-	draw(ctx){
-		var color = 0 < this.cooldown ? "#7f7f7f" : "#000000";
-		var headHeight = 50;
+    draw(ctx){
+        var color = 0 < this.cooldown ? "#7f7f7f" : "#000000";
+        var headHeight = 50;
 
-		var animFrame = 0;
-		if(0 < this.cooldown)
-			animFrame = Math.max(0, Math.min(animImages.length-1, Math.floor(4 * (1. - this.cooldown / taskWait))));
+        var animFrame = 0;
+        if(this.state === WALKING){
+            animFrame = Math.max(0, Math.min(animImages.length-1, Math.floor(4 * (1. - this.cooldown / taskWait))));
 
-		// ctx.drawImage(animImages[animFrame], this.pos[0] - 30, this.pos[1] - headHeight - 10);
-		ctx.drawImage(studentImages[Math.floor(this.frame / animFrames)], this.pos[0] - 30, this.pos[1] - headHeight - 10, 60, 134);
+            // ctx.drawImage(animImages[animFrame], this.pos[0] - 30, this.pos[1] - headHeight - 10);
+            ctx.drawImage(studentImages[Math.floor(this.frame / animFrames)],
+                this.pos[0] < this.dest ? 0 : 95, 0, 95, 214,
+                this.pos[0], this.pos[1] - headHeight - 10, 60, 134);
+        }
+        else if(this.state === SIT)
+            ctx.drawImage(studentSitImage, this.pos[0], this.pos[1] - headHeight - 10, 60, 134);
+        else
+            ctx.drawImage(studentImages[0], this.pos[0], this.pos[1] - headHeight - 10, 60, 134);
 
-		ctx.font = "15px Arial";
-		ctx.fillStyle = color;
-		ctx.fillText(this.name, this.pos[0] - 5, this.pos[1] - headHeight - 10);
+        ctx.font = "15px Arial";
+        ctx.fillStyle = color;
+        ctx.fillText(this.name, this.pos[0] - 5, this.pos[1] - headHeight - 10);
 
-	}
+    }
 }
 
 class Desk {
-	pos = [0, 0];
+    pos = [0, 0];
 
-	constructor(pos){
-		this.pos = pos;
-	}
+    constructor(pos){
+        this.pos = pos;
+    }
 
-	draw(ctx){
-		ctx.drawImage(deskImage, 0, 0, 300, 300, this.pos[0] - 30, this.pos[1], 70, 70);
-	}
+    draw(ctx, depth){
+        switch(depth){
+        case 0: ctx.drawImage(deskImage, 0, 0, 139, 182, this.pos[0], this.pos[1], 69, 91); break;
+        case 1: ctx.drawImage(chairImage, 0, 0, 139, 182, this.pos[0], this.pos[1], 69, 91); break;
+        case 2: ctx.drawImage(deskBackImage, 0, 0, 139, 182, this.pos[0], this.pos[1], 69, 91); break;
+        }
+    }
 }
 
 window.addEventListener('load', function() {
-	var asyncCheck = document.getElementById("asyncCheck");
-	if(asyncCheck){
-		asyncCheck.addEventListener("click", function(){
-			asynch = asyncCheck.checked;
-		});
-	}
+    var asyncCheck = document.getElementById("asyncCheck");
+    if(asyncCheck){
+        asyncCheck.addEventListener("click", function(){
+            asynch = asyncCheck.checked;
+        });
+    }
 
-	var multithreadCheck = document.getElementById("MultithreadCheck");
-	if(multithreadCheck){
-		multithreadCheck.addEventListener("click", function(){
-			multithread = multithreadCheck.checked;
-		});
-	}
+    var multithreadCheck = document.getElementById("MultithreadCheck");
+    if(multithreadCheck){
+        multithreadCheck.addEventListener("click", function(){
+            multithread = multithreadCheck.checked;
+        });
+    }
 
-	var edit = this.document.getElementById("taskWaitEdit");
-	if(edit){
-		edit.addEventListener("change", function(){
-			taskWait = parseFloat(edit.value);
-		});
-	}
+    var edit = this.document.getElementById("taskWaitEdit");
+    if(edit){
+        edit.addEventListener("change", function(){
+            taskWait = parseFloat(edit.value);
+        });
+    }
 
-	canvas = document.getElementById("scratch");
-	if ( ! canvas || ! canvas.getContext ) {
-		return false;
-	}
-	width = parseInt(canvas.style.width);
-	height = parseInt(canvas.style.height);
+    canvas = document.getElementById("scratch");
+    if ( ! canvas || ! canvas.getContext ) {
+        return false;
+    }
+    width = parseInt(canvas.style.width);
+    height = parseInt(canvas.style.height);
 
-	for(var i = 0; i < 4; i++){
-		animImages[i] = new Image();
-		animImages[i].src = "assets/anim_000" + i + ".png";
-	}
+    for(var i = 0; i < 4; i++){
+        animImages[i] = new Image();
+        animImages[i].src = "assets/anim_000" + i + ".png";
+    }
 
-	deskImage = new Image();
-	deskImage.src = "assets/desk.png";
+    deskImage = new Image();
+    deskImage.src = "assets/desk.png";
 
-	for(let i = 0; i < 2; i++){
-		studentImages[i] = new Image();
-		studentImages[i].src = `assets/student${i + 1}.png`;
-	}
+    chairImage = new Image();
+    chairImage.src = "assets/chair.png";
 
-	draw();
+    deskBackImage = new Image();
+    deskBackImage.src = "assets/desk-back.png";
 
-	// Animate (framerate could be subject to discuss)
-	window.setInterval(timerProc, 50);
+    for(let i = 0; i < 2; i++){
+        studentImages[i] = new Image();
+        studentImages[i].src = `assets/student${i + 1}.png`;
+    }
+
+    studentSitImage = new Image();
+    studentSitImage.src = "assets/student-sit.png";
+
+    draw();
+
+    // Animate (framerate could be subject to discuss)
+    window.setInterval(timerProc, 50);
 });
 
 var taskWait = 8;
@@ -111,148 +159,139 @@ var multithread = false;
 var asynch = false;
 var ballThrows = 0;
 var balls = [];
-var people = [...Array(4)].map((_, i) => new Person({name: "ABCD"[i], pos: [-30 + i * 100, 300], balls: [], cooldown: i === 0 ? 10 : 0}));
-const desks = [...Array(4)].map((_, i) => new Desk([i * 100, 300]));
+var people = [...Array(4)].map((_, i) => new Person({
+    id: i,
+    name: "ABCD"[i],
+    dest: i * 100 + 100,
+    pos: [-30 + i * 100, 300],
+    balls: [], cooldown: i === 0 ? 10 : 0
+}));
+const desks = [...Array(4)].map((_, i) => new Desk([i * 100 + 100, 300]));
+let tasks = [...Array(4)].map((_, i) => i);
 
 for(var i = 0; i < 10; i++){
-	var target = people[Math.floor(Math.random() * people.length)];
-	balls.push({
-		pos: [target.pos[0], target.pos[1] + Math.random() * 200],
-		velo: [0, -15 * Math.random()],
-		owner: target,
-		color: "rgb(" + (Math.random() * 255) + ", " + (Math.random() * 255) + ", " + (Math.random() * 255) + ")"
-	});
+    var target = people[Math.floor(Math.random() * people.length)];
+    balls.push({
+        pos: [target.pos[0], target.pos[1] + Math.random() * 200],
+        velo: [0, -15 * Math.random()],
+        owner: target,
+        color: "rgb(" + (Math.random() * 255) + ", " + (Math.random() * 255) + ", " + (Math.random() * 255) + ")"
+    });
 }
 
 
 
 function timerProc(){
-	animate();
-	draw();
+    animate();
+    draw();
 }
 
 function animate(){
-	var accel = 2;
+    var accel = 2;
 
-	for(var i = 0; i < balls.length;){
-		var ball = balls[i];
-		for(var j = 0; j < 2; j++)
-			ball.pos[j] += ball.velo[j];
-		// console.log("ball " + i + ": velo: " + ball.velo[1]);
-		ball.velo[1] += accel;
-		if(ball.owner === null)
-			ball.owner = people[0];
-		if(ball.owner.pos[1] - 15 < ball.pos[1]){
-			ball.owner.balls.push(ball);
-			balls.splice(i, 1);
-			continue;
-		}
-		else
-			i++;
-	}
+    for(var i = 0; i < balls.length;){
+        var ball = balls[i];
+        for(var j = 0; j < 2; j++)
+            ball.pos[j] += ball.velo[j];
+        // console.log("ball " + i + ": velo: " + ball.velo[1]);
+        ball.velo[1] += accel;
+        if(ball.owner === null)
+            ball.owner = people[0];
+        if(ball.owner.pos[1] - 15 < ball.pos[1]){
+            ball.owner.balls.push(ball);
+            balls.splice(i, 1);
+            continue;
+        }
+        else
+            i++;
+    }
 
-	for(var i = 0; i < people.length; i++){
-		var person = people[i];
-		person.animate();
-		// console.log("person " + i + ": cooldown: " + person.cooldown);
-		for(var j = 0; j < person.balls.length; j++){
-			var ball = person.balls[j];
-			ball.pos[0] = person.pos[0] - 15 - 5 * j;
-			ball.pos[1] = person.pos[1] - 25;
-		}
-		if(0 < person.cooldown){
-			person.cooldown--;
-			continue;
-		}
-		var waitingBalls = balls.reduce(function(count, ball){
-			if(ball.owner === person)
-				count++;
-			return count;
-		}, 0);
-		if((asynch || !waitingBalls) && 0 < person.balls.length){
-			var ball = person.balls.splice(0, 1)[0];
-			var leastHardWorkers = multithread ? people.filter(function(person){
-				var leastQueue = people.reduce(function(min, person){
-					return person.balls.length < min ? person.balls.length : min;
-				}, balls.length);
-				return leastQueue === person.balls.length;
-			}) : people.slice(0, 1);
-			// console.log("leastHardWorkers: " + leastHardWorkers.length);
-			var target = leastHardWorkers[Math.floor(Math.random() * leastHardWorkers.length)];
-			var speed = 10 + Math.random() * 20;
-			/// x = 1/2 * v * t^2 => t = \sqrt{2x/v}
-			var timeToArrive = (2 * speed / accel);
-			ball.owner = target;
-			ball.pos[0] = person.pos[0] + 15;
-			ball.pos[1] = person.pos[1] - 25;
-			ball.velo[0] = ((target.pos[0] - 15) - (person.pos[0] + 15)) / timeToArrive;
-			ball.velo[1] = -speed;
-			balls.push(ball);
-			person.cooldown = taskWait;
-			ballThrows++;
-			var elem = document.getElementById("ballThrows");
-			if(elem)
-				elem.innerHTML = ballThrows;
-		}
-	}
+    for(var i = 0; i < people.length; i++){
+        var person = people[i];
+        person.animate();
+        // console.log("person " + i + ": cooldown: " + person.cooldown);
+        for(var j = 0; j < person.balls.length; j++){
+            var ball = person.balls[j];
+            ball.pos[0] = person.pos[0] - 15 - 5 * j;
+            ball.pos[1] = person.pos[1] - 25;
+        }
+        if(0 < person.cooldown){
+            person.cooldown--;
+            continue;
+        }
+        var waitingBalls = balls.reduce(function(count, ball){
+            if(ball.owner === person)
+                count++;
+            return count;
+        }, 0);
+        if((asynch || !waitingBalls) && 0 < person.balls.length){
+            var ball = person.balls.splice(0, 1)[0];
+            var leastHardWorkers = multithread ? people.filter(function(person){
+                var leastQueue = people.reduce(function(min, person){
+                    return person.balls.length < min ? person.balls.length : min;
+                }, balls.length);
+                return leastQueue === person.balls.length;
+            }) : people.slice(0, 1);
+            // console.log("leastHardWorkers: " + leastHardWorkers.length);
+            var target = leastHardWorkers[Math.floor(Math.random() * leastHardWorkers.length)];
+            var speed = 10 + Math.random() * 20;
+            /// x = 1/2 * v * t^2 => t = \sqrt{2x/v}
+            var timeToArrive = (2 * speed / accel);
+            ball.owner = target;
+            ball.pos[0] = person.pos[0] + 15;
+            ball.pos[1] = person.pos[1] - 25;
+            ball.velo[0] = ((target.pos[0] - 15) - (person.pos[0] + 15)) / timeToArrive;
+            ball.velo[1] = -speed;
+            balls.push(ball);
+            person.cooldown = taskWait;
+            ballThrows++;
+            var elem = document.getElementById("ballThrows");
+            if(elem)
+                elem.innerHTML = ballThrows;
+        }
+    }
 }
 
 function draw() {
-	var ctx = canvas.getContext('2d');
+    var ctx = canvas.getContext('2d');
 
-	// Draw person shape on pos, annotated by name with color.
-	// Pos will be the position of feet.
-	function drawPerson(person, color){
-		var {pos, name} = person;
-		var color = 0 < person.cooldown ? "#7f7f7f" : "#000000";
-		var headHeight = 50;
-		var headRadius = 10;
-		var handLength = 15;
+    function drawBall(ball){
+        ctx.beginPath();
+        ctx.arc(ball.pos[0], ball.pos[1], ballRadius, 0, 2*Math.PI);
+        ctx.fillStyle = ball.color;
+        ctx.fill();
+        ctx.strokeStyle = "#000";
+        ctx.stroke();
+    }
 
-		var animFrame = 0;
-		if(0 < person.cooldown)
-			animFrame = Math.max(0, Math.min(animImages.length-1, Math.floor(4 * (1. - person.cooldown / taskWait))));
+    function setIdentity(){
+        ctx.setTransform(1, 0, 0, 1, 0, 0);
+    }
 
-		ctx.drawImage(animImages[animFrame], pos[0] - 30, pos[1] - headHeight - 10);
+    var ballRadius = 5;
+    var offset = 0;
 
-		ctx.font = "15px Arial";
-		ctx.fillStyle = color;
-		ctx.fillText(name, pos[0] - 5, pos[1] - headHeight - 10);
+    ctx.clearRect(0,0,width,height);
+    ctx.setTransform(1,0,0,1, offset, 0);
 
-		for(var i = 0; i < person.balls.length; i++)
-			drawBall(person.balls[i]);
-	}
+    for(let desk of desks)
+        desk.draw(ctx, 2);
 
-	function drawBall(ball){
-		ctx.beginPath();
-		ctx.arc(ball.pos[0], ball.pos[1], ballRadius, 0, 2*Math.PI);
-		ctx.fillStyle = ball.color;
-		ctx.fill();
-		ctx.strokeStyle = "#000";
-		ctx.stroke();
-	}
+    for(let desk of desks)
+        desk.draw(ctx, 1);
 
-	function setIdentity(){
-		ctx.setTransform(1, 0, 0, 1, 0, 0);
-	}
+    // Draw people (represents threads)
+    for(var i = 0; i < people.length; i++)
+        people[i].draw(ctx);
 
-	var ballRadius = 5;
-	var offset = 100;
+    for(let desk of desks)
+        desk.draw(ctx, 0);
 
-	ctx.clearRect(0,0,width,height);
-	ctx.setTransform(1,0,0,1, offset, 0);
 
-	// Draw people (represents threads)
-	for(var i = 0; i < people.length; i++)
-		people[i].draw(ctx);
+    for(var i = 0; i < balls.length; i++){
+        drawBall(balls[i]);
+    }
 
-	for(let desk of desks)
-		desk.draw(ctx);
-
-	for(var i = 0; i < balls.length; i++){
-		drawBall(balls[i]);
-	}
-
-	setIdentity();
+    setIdentity();
 }
 })();
