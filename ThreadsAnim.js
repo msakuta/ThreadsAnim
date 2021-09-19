7693d5c57b5eab76208e32e0457ee3c3fdaffbac
msakuta <masahiro.sakuta@gmail.com>
2021-09-19 21:36:36 +0900

    Actually throw balls bwtween mainThread and workerThreads

--- ThreadsAnim.js
+++ ThreadsAnim.js
@@ -10,7 +10,10 @@ let deskBackImage;
 let studentImages = [];
 let studentSitImage;
 
+const accel = 2;
 const animFrames = 5;
+const ballRadius = 5;
+const deskOffset = [30, 35];
 
 const IDLE = 0;
 const WALKING = 1;
@@ -25,6 +28,7 @@ class Person {
     balls = [];
     frame = 0;
     state = IDLE;
+    heldBall = null;
 
     constructor(params){
         this.id = params.id;
@@ -41,18 +45,29 @@ class Person {
             if(Math.abs(this.pos[0] - this.dest) < walkSpeed){
                 this.state = this.dest === 0 ? IDLE : SIT;
                 this.pos[0] = this.dest;
+                this.cooldown = 0;
             }
             else
                 this.pos[0] = (this.pos[0] + (this.pos[0] < this.dest ? walkSpeed : -walkSpeed) + width) % width;
         }
         else if(this.state === SIT/* && this.balls.length === 0*/){
             if(--this.cooldown <= 0){
-                if(tasks.length === 0){
+                if(this.heldBall){
+                    this.heldBall.throw(mainThread, this);
+                    this.heldBall = null;
+                }
+                if(this.balls.length !== 0){
+                    this.heldBall = this.balls.pop();
+                    this.cooldown = Math.random() * 50 + 100;
+                }
+                else if(tasks.length === 0){
                     this.dest = 0;
                     this.state = WALKING;
                 }
                 else{
                     // fetch task
+                    let ball = new Ball(this);
+                    ball.throw(this, mainThread);
                     this.cooldown += 100;
                     tasks.pop();
                 }
@@ -67,9 +82,25 @@ class Person {
             walkCooldown--;
         }
         this.frame = (this.frame + 1) % (2 * animFrames);
+
+        for(let j = 0; j < this.balls.length; j++){
+            let ball = this.balls[j];
+            ball.pos[0] = this.pos[0] + deskOffset[0] - 5 * j;
+            ball.pos[1] = this.pos[1] + deskOffset[1];
+        }
     }
 
-    draw(ctx){
+    draw(ctx, depth){
+        if(depth === 0){
+            for(let i = 0; i < this.balls.length; i++)
+                drawBall(this.balls[i], ctx);
+            if(this.heldBall)
+                drawBall(this.heldBall, ctx);
+            return;
+        }
+        else if(depth !== 1){
+            return;
+        }
         var color = 0 < this.cooldown ? "#7f7f7f" : "#000000";
         var headHeight = 40;
 
@@ -87,15 +118,15 @@ class Person {
                 0, 0, 60, 134);
             ctx.restore();
         }
-        else if(this.state === SIT)
+        else if(this.state === SIT){
             ctx.drawImage(studentSitImage, this.pos[0], this.pos[1] - headHeight - 10, 60, 134);
+        }
         else
             ctx.drawImage(studentImages[0], this.pos[0], this.pos[1] - headHeight - 10, 60, 134);
 
         ctx.font = "15px Arial";
         ctx.fillStyle = color;
         ctx.fillText(this.name, this.pos[0] + 30, this.pos[1] - headHeight - 10);
-
     }
 }
 
@@ -112,6 +143,34 @@ class Desk {
         case 1: ctx.drawImage(chairImage, 0, 0, 139, 182, this.pos[0], this.pos[1], 69, 91); break;
         case 2: ctx.drawImage(deskBackImage, 0, 0, 139, 182, this.pos[0], this.pos[1], 69, 91); break;
         }
+    }
+}
+
+class Ball {
+    pos = [0, 0];
+    velo = [0, -15 * Math.random()];
+    owner = null;
+    color = "rgb(" + (Math.random() * 255) + ", " + (Math.random() * 255) + ", " + (Math.random() * 255) + ")";
+
+    constructor(target){
+        this.pos = [target.pos[0], target.pos[1] + Math.random() * 200];
+        this.owner = target;
+    }
+
+    animate(){
+
+    }
+
+    throw(target, source){
+        const speed = 10 + Math.random() * 20;
+        /// x = 1/2 * v * t^2 => t = \sqrt{2x/v}
+        const timeToArrive = (2 * speed / accel);
+        this.owner = target;
+        this.pos[0] = source.pos[0] + deskOffset[0];
+        this.pos[1] = source.pos[1] + deskOffset[1];
+        this.velo[0] = ((target.pos[0]) - (source.pos[0])) / timeToArrive;
+        this.velo[1] = -speed;
+        balls.push(this);
     }
 }
 
@@ -184,19 +243,22 @@ var people = [...Array(4)].map((_, i) => new Person({
     pos: [0, 300],
     balls: [], cooldown: i === 0 ? 10 : 0
 }));
+let mainThread = new Person({
+    id: -1,
+    name: "mainThread",
+    dest: 0,
+    pos: [0, 300],
+    balls: [],
+});
+
 const desks = [...Array(4)].map((_, i) => new Desk([i * 100 + 100, 300]));
 let tasks = [...Array(4)].map((_, i) => i);
 let walkCooldown = 0;
 
-for(var i = 0; i < 10; i++){
-    var target = people[Math.floor(Math.random() * people.length)];
-    balls.push({
-        pos: [target.pos[0], target.pos[1] + Math.random() * 200],
-        velo: [0, -15 * Math.random()],
-        owner: target,
-        color: "rgb(" + (Math.random() * 255) + ", " + (Math.random() * 255) + ", " + (Math.random() * 255) + ")"
-    });
-}
+// for(var i = 0; i < 10; i++){
+//     var target = people[Math.floor(Math.random() * people.length)];
+//     balls.push(new Ball(target));
+// }
 
 
 
@@ -206,7 +268,6 @@ function timerProc(){
 }
 
 function animate(){
-    var accel = 2;
 
     for(var i = 0; i < balls.length;){
         var ball = balls[i];
@@ -214,9 +275,9 @@ function animate(){
             ball.pos[j] += ball.velo[j];
         // console.log("ball " + i + ": velo: " + ball.velo[1]);
         ball.velo[1] += accel;
-        if(ball.owner === null)
-            ball.owner = people[0];
-        if(ball.owner.pos[1] - 15 < ball.pos[1]){
+        // if(ball.owner === null)
+        //     ball.owner = people[0];
+        if(ball.owner.pos[1] + deskOffset[0] < ball.pos[1]){
             ball.owner.balls.push(ball);
             balls.splice(i, 1);
             continue;
@@ -229,11 +290,6 @@ function animate(){
         var person = people[i];
         person.animate();
         // console.log("person " + i + ": cooldown: " + person.cooldown);
-        for(var j = 0; j < person.balls.length; j++){
-            var ball = person.balls[j];
-            ball.pos[0] = person.pos[0] - 15 - 5 * j;
-            ball.pos[1] = person.pos[1] - 25;
-        }
         if(0 < person.cooldown){
             person.cooldown--;
             continue;
@@ -275,23 +331,22 @@ function animate(){
     }
 }
 
+function drawBall(ball, ctx){
+    ctx.beginPath();
+    ctx.arc(ball.pos[0], ball.pos[1], ballRadius, 0, 2*Math.PI);
+    ctx.fillStyle = ball.color;
+    ctx.fill();
+    ctx.strokeStyle = "#000";
+    ctx.stroke();
+}
+
 function draw() {
     var ctx = canvas.getContext('2d');
-
-    function drawBall(ball){
-        ctx.beginPath();
-        ctx.arc(ball.pos[0], ball.pos[1], ballRadius, 0, 2*Math.PI);
-        ctx.fillStyle = ball.color;
-        ctx.fill();
-        ctx.strokeStyle = "#000";
-        ctx.stroke();
-    }
 
     function setIdentity(){
         ctx.setTransform(1, 0, 0, 1, 0, 0);
     }
 
-    var ballRadius = 5;
     var offset = 0;
 
     ctx.clearRect(0,0,width,height);
@@ -305,14 +360,16 @@ function draw() {
 
     // Draw people (represents threads)
     for(var i = 0; i < people.length; i++)
-        people[i].draw(ctx);
+        people[i].draw(ctx, 1);
 
     for(let desk of desks)
         desk.draw(ctx, 0);
 
+    for(var i = 0; i < people.length; i++)
+        people[i].draw(ctx, 0);
 
     for(var i = 0; i < balls.length; i++){
-        drawBall(balls[i]);
+        drawBall(balls[i], ctx);
     }
 
     setIdentity();
